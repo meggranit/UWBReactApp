4bc3806af07a5d06605d4293d351f6e4dea529e6
meggranit <meghangranit@msn.com>
2023-01-18 17:26:38 -0500

    fix port variable name

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const db = require('./connection')
 const app = express();
 const path = require("path");
 
+
 app.use(express.json());
 const phoneRoutes = require('./routes/phoneRoutes')
 const roomOneRoutes = require('./routes/roomOneRoutes')
 app.use("/phone/", phoneRoutes);
 app.use("/roomone/", roomOneRoutes)
 app.get("/", (req, res) => {
-    res.send("Server working on port " + port);
+    res.send("Server working on port " + PORT);
 });
 
 app.use(express.static(path.resolve(__dirname, "./client/build")));
