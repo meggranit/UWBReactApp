cf8cfc5c7cd6f490537542696200a60bb07f3d1c
meggranit <meghangranit@msn.com>
2023-03-05 18:07:44 -0500

    add pigeon maps and show rooms on map

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,14 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { Map, Marker, Overlay } from "pigeon-maps"
 
 
 export default function Home() {
-    
+    const [center, setCenter] = useState([40.277635, -74.005053])
+    const [zoom, setZoom] = useState(18)
+    const [hue, setHue] = useState(0)
+    const color = `hsl(${hue % 360}deg 39% 70%)`
     return (
-        <div>
-           <p>Test</p>
-        </div>
+        
+ 
+    <Map 
+      height={300}
+      center={center} 
+      zoom={zoom} 
+      onBoundsChanged={({ center, zoom }) => { 
+        setCenter(center) 
+        setZoom(zoom) 
+      }} 
+    >
+      <Overlay anchor={[40.277635, -74.005053]} >
+        <p>Room 1</p>
+      </Overlay>
+      <Overlay anchor={[40.277762, -74.005109]} >
+      <p>Room 2</p>
+      </Overlay>
+      <Overlay anchor={[40.277856, -74.005173]} >
+      <p>Room 3</p>
+      </Overlay>
+      <Overlay anchor={[40.277991, -74.005162]} >
+       <p>Room 4</p>
+      </Overlay>
+    </Map>
+
+        
     )
 }
 
