1399ed66d63655a8fe4ca20ce2c261ef427f0cc2
meggranit <meghangranit@msn.com>
2023-04-01 11:49:21 -0400

    select room id

--- server/routes/sensorRoutes.js
+++ server/routes/sensorRoutes.js
@@ -9,8 +9,8 @@ router.post("/getroomid", async(req, res) => {
 
     const sensorID = req.body.sensorID
     try {
-        const sensorData = await sensorModel.find({ 'sensorID': sensorID });
-        res.send(sensorData.roomID)
+        const sensorData = await sensorModel.find({ 'sensorID': sensorID }).select('roomID');
+        res.send(sensorData)
         console.log(sensorData)
     } catch (error) {
         return res.status(400).json({ message: error });
