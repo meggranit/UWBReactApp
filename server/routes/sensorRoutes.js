4333d73635cc62f3ad5c8288332f1e044496b861
meggranit <meghangranit@msn.com>
2023-04-01 11:37:57 -0400

    get room id from sesnor id

--- server/routes/sensorRoutes.js
+++ server/routes/sensorRoutes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const { getSensors } = require('../controllers/sensorsController.js')
+const sensorModel = require("../models/sensorModel");
 
 router.get('/', getSensors)
+
+router.post("/getroomid", async(req, res) => {
+
+    const sesnorID = req.body.sensorID
+    try {
+        const sensorData = await sesnorModel.find({ 'sensorID': sensorID });
+        res.send(sensorData)
+        console.log(sensorData)
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
+   });
 
 
 module.exports = router
