af8a6abe21f4642f1dfd936ae131a4aba61f179e
meggranit <meghangranit@msn.com>
2023-04-17 12:20:57 -0400

    console log for testing

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -81,6 +81,8 @@ router.post("/newreport", async(req, res) => {
             
             res.send(roomData)
             console.log(roomData)
+            console.log("PREVIOS RECORD " + previousRecord)
+            
             
     
         } catch (error) {
