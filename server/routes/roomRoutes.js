521355ec45d604a53814068d8100b7c9715fda0a
meggranit <meghangranit@msn.com>
2023-04-19 11:50:46 -0400

    test- only post api if device doesnt exist

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -68,7 +68,7 @@ router.post("/newreport", async(req, res) => {
     }
 
     //if they have a previous record, they moved to different room, and they are further from new sensor than previous sensor, dont create record
-    if(previousRecord && moveRooms && furtherFromSensor){
+    if(previousRecord /* && moveRooms && furtherFromSensor */){
         console.log("Don't insert into database, sensor from another room is picking up phone")
     } else {
 
