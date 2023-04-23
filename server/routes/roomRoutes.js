d7efebfd9d33389697cb32070437dc5b14162f83
meggranit <meghangranit@msn.com>
2023-04-23 13:36:33 -0400

    test nesting conditional

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -68,32 +68,35 @@ router.post("/newreport", async(req, res) => {
         } else {
             furtherFromSensor = false
         }
+
+        if(moveRooms && furtherFromSensor){
+            console.log("Don't insert into database, sensor from another room is picking up phone")
+        } else {
+    
+            try {
+    
+                //updateOne(data , update , options)
+                pusher.trigger("channel_room1", "event_room1", { message: "post room" });
+                const roomData = await roomModel.updateOne( { 'deviceID' : deviceID} , { $set: { tagID: tagID, buildingID: buildingID, roomID: roomID , lat: lat, long : long ,    time : time,  distance: distance,   deviceID: deviceID }} , {upsert : true} )
+                const phoneData = await phoneModel.updateOne( { 'deviceID' : deviceID} , { $set: { deviceID: deviceID , roomID: roomID , distance: distance }} , {upsert : true} )
+                
+                res.send(roomData)
+                console.log(roomData)
+                console.log("PREVIOUS RECORD " + previousRecord)
+               
+                
+                
+        
+            } catch (error) {
+                return res.status(400).json({ message: error });
+            }
+        }
+       
+
     })
 
 
     //if they have a previous record, they moved to different room, and they are further from new sensor than previous sensor, dont create record
-    if(moveRooms && furtherFromSensor){
-        console.log("Don't insert into database, sensor from another room is picking up phone")
-    } else {
-
-        try {
-
-            //updateOne(data , update , options)
-            pusher.trigger("channel_room1", "event_room1", { message: "post room" });
-            const roomData = await roomModel.updateOne( { 'deviceID' : deviceID} , { $set: { tagID: tagID, buildingID: buildingID, roomID: roomID , lat: lat, long : long ,    time : time,  distance: distance,   deviceID: deviceID }} , {upsert : true} )
-            const phoneData = await phoneModel.updateOne( { 'deviceID' : deviceID} , { $set: { deviceID: deviceID , roomID: roomID , distance: distance }} , {upsert : true} )
-            
-            res.send(roomData)
-            console.log(roomData)
-            console.log("PREVIOUS RECORD " + previousRecord)
-           
-            
-            
-    
-        } catch (error) {
-            return res.status(400).json({ message: error });
-        }
-    }
    
 
     
